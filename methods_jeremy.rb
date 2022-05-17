33ef2dbeddbbfcda49e20eabf73d8cec6223effa
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-17 08:09:32 -0400

    Finished password checker challenge

--- methods_jeremy.rb
+++ methods_jeremy.rb
@@ -43,19 +43,50 @@ is_palindrome('racecar')
 is_palindrome('hello')
 
 # ## Challenge: Password Checker
-
 # ### User Stories
-
 # You are writing the user registration page for a secure web site.
 # On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
-
 # - As a developer, I can create a method that checks for the following rules for a user ID and password:
 #   - User ID and password _cannot_ be the same.
-#   - User ID and password _must_ be at least six characters long.
+#    - User ID and password _must_ be at least six characters long.
 #   - Password _must_ contain at least one of: !#$
 #   - User ID _cannot_ contain the following characters: !#$ or spaces
 #   - Password _cannot_ be the word "password".
+def check_password(user_id, password)
+  if user_id == password
+    p 'User ID and Password cannot be the same!'
 
+  elsif user_id.length < 6 ||
+        password.length < 6
+    p 'User ID and Password must be at least six characters!'
+
+  elsif !(password.include?('!') ||
+          password.include?('#') ||
+          password.include?('$'))
+    p 'Password must contain at least one !, #, or $ in it!'
+
+  elsif user_id.include?('!') ||
+        user_id.include?('#') ||
+        user_id.include?('$') ||
+        user_id.include?(' ')
+    p 'User ID cannot have !, #, or $ in it!'
+
+  elsif password.include?('password')
+    p "Password cannot contain 'password'!"
+
+  else
+    p "User ID #{user_id} and password #{password} are valid."
+  end
+end
+
+# Tests//////////////////////////////
+check_password('jduncan!', 'jduncan!!')
+check_password('jduncan', 'password')
+check_password('jduncan', 'jduncan')
+check_password('jasdasad', 'ada')
+check_password('jduncan', 'password1234')
+check_password('jduncan', 'password!')
+check_password('jduncan', '#6asdAPSSPD')
 # ### User Stories: Stretch
 
 # - As a user, I can enter my user ID and password into the terminal after being prompted to find out if the they are acceptable.
