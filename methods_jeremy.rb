d9cdac91756ec99f0a67b3c1235ee6be3a77dcac
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-16 23:13:38 -0400

    Completed first challenge

--- methods_jeremy.rb
+++ methods_jeremy.rb
@@ -1,6 +1,12 @@
 # ## Challenges
 
 # - Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+def sum_these_numbers(num1, num2)
+  p num1 + num2
+end
+
+sum_these_numbers(15, 55)
+
 # - Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
 # - Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
 # - Create a method that takes in a string and determines if the string is a palindrome.
