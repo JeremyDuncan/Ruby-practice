69d3c188917ce746ac5f43d2e3f06d728836aef5
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-17 11:15:19 -0400

    Completed more challenges

--- blocks_jeremy.rb
+++ blocks_jeremy.rb
@@ -8,7 +8,22 @@ num = 1
 end
 
 # - Write a loop that prints the numbers 1 to 20 using a different approach than previously used.
+num_range = 1..20
+num_range.each do |value|
+  p value
+end
+
+num_range.each { |value| p value }
+
 # - Write a loop that prints only even numbers from 20 to 0.
+num_range.each do |value|
+  if value.even?
+    p "#{value} is even"
+  else
+    p "#{value} is odd"
+  end
+end
+
 # - Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
 # - Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
 # - Create a method that takes in an array of numbers and returns the largest number in the array.
