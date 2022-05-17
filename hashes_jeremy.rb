b8d7b2275a1ecabc5c40eb91758939f841e06bd3
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-17 19:44:55 -0400

    Completed Challenge

--- hashes_jeremy.rb
+++ hashes_jeremy.rb
@@ -17,10 +17,16 @@ puts my_phone
 p my_phone[:Wordle]
 
 # - As a developer, I can update two keys in my_phone.
+my_phone[:Wordlee] = my_phone.delete :Wordle
+my_phone[:Maps] = my_phone.delete :"Google Maps"
+
+p my_phone
+
+# - As a developer, I can update two values in my_phone.
 my_phone[:"Candy Crush"] = 'The levels never end!'
 my_phone[:Chrome] = 'Invades your privacy..Thanks Google'
 
-# - As a developer, I can update two values in my_phone.
+p my_phone
 
 # - As a developer, I can delete two key:value pairs from my_phone.
 # - As a developer, I can use an enumerable method to return information about all of my_phone's applications.
