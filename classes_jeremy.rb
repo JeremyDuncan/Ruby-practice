e98ae81c02eb5625d86c875a3510790be1456af8
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-17 22:39:41 -0400

    Completed 2 challenges

--- classes_jeremy.rb
+++ classes_jeremy.rb
@@ -1,9 +1,19 @@
 # ### Challenges
 # #### For the following Task challenge, use initialize and getter methods in your class
 # - As a developer, I can create a class called Task.
+# - As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
 class Task
   def initialize(title)
     @title = title
+    @status = 'incomplete'
+  end
+
+  def set_status(status)
+    @status = status
+  end
+
+  def get_status
+    @status
   end
 
   def get_title
@@ -25,8 +35,10 @@ p wednesday.get_title
 p thursday.get_title
 p friday.get_title
 
-# - As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
 # - As a developer, I can update the status of each instance of class Task when the task has been completed.
+p friday.get_status
+friday.set_status('Finished')
+p friday.get_status
 
 # #### For the following ColorPalette challenge use initialize and `attr_accessor` methods in your class
 # - As a developer, I can create a class called ColorPalette.
