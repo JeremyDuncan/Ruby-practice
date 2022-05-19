31e926982391728356d04c8f5ede5f89edf0548d
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-19 18:35:02 -0400

    Completed challenges

--- rspec-jeremy/rspec.rb
+++ rspec-jeremy/rspec.rb
@@ -17,7 +17,7 @@ class Task
     @title = 'no title added'
     @description = 'no description added'
     @progress = 'in progress'
-    @due_date = 'no due date'
+    @due_date = Date.new(9999, 5, 5)
   end
 
   def deadline(date)
@@ -40,8 +40,7 @@ class TaskList
   def by_due_date
     due = nil
     output = []
-
-    sorted = @to_do.sort_by { |obj| obj.due_date }
+    sorted = @to_do.sort { |obj1, obj2| obj1.due_date <=> obj2.due_date }
     due = sorted.select { |i| i.progress == 'in progress' }
     due.each do |key|
       output << key.title
@@ -83,26 +82,3 @@ class TaskList
     output
   end
 end
-
-# Story: As a developer, I can give a Task a title and retrieve it.
-
-# Story: As a developer, I can give a Task a description and retrieve it.
-
-# Story: As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
-
-# Story: As a developer, when I print a Task that is done, its status is shown.
-
-# Story: As a developer, I can add all of my Tasks to a TaskList.
-
-# Story: As a developer with a TaskList, I can print the completed items.
-
-# Story: As a developer with a TaskList, I can print the incomplete items.
-
-# Stretch: Due Date
-# Story: As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
-
-# Story: As a developer with a TaskList, I can list all the not completed items that are due today.
-
-# Story: As a developer with a TaskList, I can list all the not completed items in order of due date.
-
-# Story: As a developer with a TaskList with and without due dates, I can list all the not completed items in order of due date, and then the items without due dates.
